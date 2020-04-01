1485ac8e58294c1c39262eccdafe850c50a2da00
NeverSettels <settels.eric@gmail.com>
2020-04-01 10:23:43 -0700

    add variables input and output in scripts.js file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 //backend
 function pigLatin(str) {
-
+  return str
 }
 
 //front end
 $(document).ready(function () {
+  $('#sentence').submit(function (event) {
+    event.preventDefault();
+    var input = $("#input").val();
+    var output = pigLatin(input);
+    $("#result").removeClass('no-show')
+    $('#result').text(output)
+  })
 
 })
