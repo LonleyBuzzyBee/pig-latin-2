886f4b06122702cb91a59507c8bf0cc4ea65ebc0
NeverSettels <settels.eric@gmail.com>
2020-04-01 10:51:17 -0700

    got first spec in readme to work in scripts.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 //backend
 function pigLatin(str) {
-  return str
+  var vowelsArr = ['a', 'e', 'i', 'o', 'u']
+  var splitStr = str.split(' ')
+  var final = []
+  splitStr.forEach(function (word) {
+    if (vowelsArr.includes(word[0])) {
+      final.push(word + 'ay')
+    } else {
+      final.push(word)
+    }
+  });
+  // console.log(splitStr[0], splitStr[0][0]);
+  return final.join(' ')
 }
 
 //front end
@@ -9,8 +20,8 @@ $(document).ready(function () {
     event.preventDefault();
     var input = $("#input").val();
     var output = pigLatin(input);
-    $("#result").removeClass('no-show')
-    $('#result').text(output)
+    $("#result-box").slideToggle('.no-show');
+    $('#result').text(output);
   })
 
 })
